e44e7dbf53b5e90e7cb94237681c0992396d5fec
Vintius <46298999+Vintius@users.noreply.github.com>
2020-02-25 21:19:41 +0300

    Optimize javascript file

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     // menu
-    var menuLinks = document.body.querySelectorAll('.js-scroll-link'),
+    let menuLinks = document.body.querySelectorAll('.js-scroll-link'),
         menuLinksLength = menuLinks.length,
         menu = document.body.querySelector(".menu-wrapper");
 
 
     function clearClasses(items) {
-        for (var i = 0; i < items.length; i++) {
+        for (let i = 0; i < items.length; i++) {
             items[i].classList.remove('is-active');
         }
     }
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function addClasses(event) {
         event.preventDefault();
 
-        var href = this.getAttribute("href").slice(1),
+        let href = this.getAttribute("href").slice(1),
             target = document.getElementById(href),
             position = target.getBoundingClientRect(),
             menuHeight = menu.offsetHeight;
@@ -25,17 +25,17 @@ document.addEventListener("DOMContentLoaded", function() {
         window.scrollTo(0, position.top - menuHeight);
     }
 
-    for (var i = 0; i < menuLinksLength; i++) {
+    for (let i = 0; i < menuLinksLength; i++) {
         menuLinks[i].addEventListener('click', addClasses);
     }
 
     // tooltip
-    var tooltips = document.body.querySelectorAll(".js-tooltip-link");
+    let tooltips = document.body.querySelectorAll(".js-tooltip-link");
 
     function toggleTooltip(event) {
         event.preventDefault();
 
-        var content = this.querySelector(".js-tooltip-content");
+        let content = this.querySelector(".js-tooltip-content");
 
         if (content.classList.contains("is-active")) {
             content.classList.remove('is-active');
@@ -45,17 +45,17 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    for (var i = 0; i < tooltips.length; i++) {
+    for (let i = 0; i < tooltips.length; i++) {
         tooltips[i].addEventListener('click', toggleTooltip);
     }
-    
+
     // questions
-    var questions = document.body.querySelectorAll(".js-toggle-question");
+    let questions = document.body.querySelectorAll(".js-toggle-question");
 
     function toggleQuestion(event) {
         event.preventDefault();
 
-        var parent = this.closest(".faq-list");
+        let parent = this.closest(".faq-list");
 
         if (parent.classList.contains("is-active")) {
             parent.classList.remove('is-active');
@@ -65,21 +65,21 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    for (var i = 0; i < questions.length; i++) {
+    for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', toggleQuestion);
     }
-    
+
     // tabs
-    var tabLinks = document.body.querySelectorAll('.js-tab-link'),
+    let tabLinks = document.body.querySelectorAll('.js-tab-link'),
         tabContents = document.body.querySelectorAll('.js-tab-content');
 
     function updateTabs(event) {
         event.preventDefault();
 
-        var tabTarget = this.dataset.target,
+        let tabTarget = this.dataset.target,
             target = [];
 
-        for (var i = 0; i < tabContents.length; i++) {
+        for (let i = 0; i < tabContents.length; i++) {
             if (tabTarget === tabContents[i].dataset.target) {
                 target.push(tabContents[i]);
             }
@@ -91,7 +91,7 @@ document.addEventListener("DOMContentLoaded", function() {
         target[0].classList.add('is-active');
     }
 
-    for (var i = 0; i < tabLinks.length; i++) {
+    for (let i = 0; i < tabLinks.length; i++) {
         tabLinks[i].addEventListener('click', updateTabs);
     }
 });
